f889ccd4c56bdb8b821ece5a0d7c02b0af29ac76
L00170570 <L00170570@S-CJ5Y9Q3>
2025-03-21 11:34:05 +0000

    recetionist Hopme page started

--- Dental_Surgery/Pages/Index.cshtml.cs
+++ Dental_Surgery/Pages/Index.cshtml.cs
@@ -1,20 +1,39 @@
+using Dental.DataAccess;
+using Dental.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dental_Surgery.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly AppDBContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(AppDBContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        [BindProperty(SupportsGet = true)]
+        public DateTime ScheduleDate { get; set; } = DateTime.Today;
 
+        public List<IGrouping<DateTime, Appointment>> GroupedAppointments { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            var allAppointments = await _context.Appointments
+                .Include(a => a.Dentist)
+                .Include(a => a.Patient)
+                .Include(a => a.Treatment)
+                .Where(a => a.AppointmentDate >= ScheduleDate)
+                .OrderBy(a => a.AppointmentDate)
+                .ToListAsync();
+
+            GroupedAppointments = allAppointments
+                .GroupBy(a => a.AppointmentDate.Date)
+                .OrderBy(g => g.Key)
+                .ToList();
         }
     }
 }
