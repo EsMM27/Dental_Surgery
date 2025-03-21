77aee354fe882fa3608a78c253b8cb0960c1e93f
Edgar <44414317+EsMM27@users.noreply.github.com>
2025-03-21 00:27:01 +0000

    Worked on index pages for Treatments,Receptionist,Layout Created User management pages with crud ops to create and delete users, Created simple privacy page, moved styles to Nav,site

---
+++ Dental_Surgery/Pages/UserManagment/Delete.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dental_Surgery.Pages.UserManagment
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DeleteModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public IdentityUser User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _userManager.FindByIdAsync(id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _userManager.FindByIdAsync(id);
+
+            if (User != null)
+            {
+                var result = await _userManager.DeleteAsync(User);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("./Index");
+                }
+            }
+
+            return Page();
+        }
+    }
+}
