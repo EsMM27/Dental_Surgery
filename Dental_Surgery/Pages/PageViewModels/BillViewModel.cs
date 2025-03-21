c513612e4a9d8f8b553e45848296c97021c5962b
Edgar <44414317+EsMM27@users.noreply.github.com>
2025-03-21 16:25:24 +0000

    Worked on Bill generation

---
+++ Dental_Surgery/Pages/PageViewModels/BillViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Dental_Surgery.Pages.PageViewModels
+{
+    public class BillViewModel
+    {
+        public string PatientName { get; set; }
+        public string TreatmentName { get; set; }
+        public decimal TreatmentCost { get; set; }
+        public int UnattendedAppointmentsCount { get; set; }
+        public decimal UnattendedAppointmentCharge { get; set; }
+        public decimal TotalCost { get; set; }
+        public string PatientPhone { get; set; }
+        public string PatientAddress { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
